6588f5cc1ea75432a13a84ffc00b116be2e31ccf
Julian Pineda <miroamarillo@gmail.com>
2016-03-21 21:06:30 -0400

    updating bodyParser

--- nodeLab/server.js
+++ nodeLab/server.js
@@ -4,10 +4,12 @@ var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var app            = express();
 
-app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); 				// log every request to the console
-app.use(bodyParser()); 					// pull information from html in POST
-app.use(methodOverride()); 				// simulate DELETE and PUT
+app.use(express.static(__dirname + '/public'));         // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                 // log every request to the console
+app.use(bodyParser.urlencoded({                         // pull information from html in POST
+        extended: true
+}));
+app.use(methodOverride());                              // simulate DELETE and PUT
 
-app.listen(3000);	
-console.log('Simple static server listening on port 3000'); 
+app.listen(3000);
+console.log('Simple static server listening on port 3000');
